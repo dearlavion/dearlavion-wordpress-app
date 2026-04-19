4f8b5315a50f64ab6e593e0fbe974e7c3f33489c
alysson <dearlavion@gmail.com>
2026-04-19 20:27:11 +0800

    save ACF fields as json

--- wordpress/wp-content/themes/astra-child/functions.php
+++ wordpress/wp-content/themes/astra-child/functions.php
@@ -11,3 +11,15 @@ add_action('wp_enqueue_scripts', function () {
         ['astra-parent']
     );
 });
+
+// Save ACF field groups to theme
+add_filter('acf/settings/save_json', function($path) {
+    return get_stylesheet_directory() . '/acf-json';
+});
+
+// Load ACF field groups from theme
+add_filter('acf/settings/load_json', function($paths) {
+    unset($paths[0]); // remove default
+    $paths[] = get_stylesheet_directory() . '/acf-json';
+    return $paths;
+});
