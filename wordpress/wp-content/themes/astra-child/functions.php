798f2d64a7d01b56de9367b069c8589b6541a9f4
alysson <dearlavion@gmail.com>
2026-04-19 20:22:56 +0800

    astra child

---
+++ wordpress/wp-content/themes/astra-child/functions.php
@@ -0,0 +1,13 @@
+<?php
+add_action('wp_enqueue_scripts', function () {
+    wp_enqueue_style(
+        'astra-parent',
+        get_template_directory_uri() . '/style.css'
+    );
+
+    wp_enqueue_style(
+        'astra-child',
+        get_stylesheet_uri(),
+        ['astra-parent']
+    );
+});
